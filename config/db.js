7bb59c18dcf2c042f0c5b905b875c729fbb6e9e4
unknown <cristian.perez.loezar@gmail.com>
2025-06-19 00:53:16 -0400

    vamos que se puede3

--- config/db.js
+++ config/db.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: false, // Esto es necesario para Supabase
+    rejectUnauthorized: false // Importante para Supabase
   },
-  allowExitOnIdle: true,
+  allowExitOnIdle: true
 });
-
-module.exports = pool;
 
 
 
